603714fee6aa6fbe0aceab248f542e1c89a4a860
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-02-13 00:07:53 +0530

    Improving the hook to improve the response time and optimistic updates - FASTER

--- src/hooks/use-projects.ts
+++ src/hooks/use-projects.ts
@@ -1,5 +1,10 @@
+/* eslint-disable react-hooks/purity */
+
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+import { useAuth } from "@clerk/nextjs";
+
 
 
 export const useProjects = () => {
@@ -13,5 +18,26 @@ export const useProjectsPartial = (limit: number) => {
 }
 
 export const useCreateProject = () => {
-    return useMutation(api.projects.create)
+    return useMutation(api.projects.create).withOptimisticUpdate(
+        (localStore , args) => {
+            const existingProjects = localStore.getQuery(api.projects.get)
+
+            if(existingProjects != undefined){
+                const now = Date.now();
+                const newProject = {
+                    _id: crypto.randomUUID() as Id<"projects">,
+                    _creationTime: now,
+                    name: args.name,
+                    ownerId: "anonymous",
+                    updatedAt: now,
+                }
+
+                localStore.setQuery(api.projects.get, {}, [
+                newProject,
+                ...existingProjects
+                ])
+            };
+
+        }
+    )
 }
