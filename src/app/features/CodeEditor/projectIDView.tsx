05b0374f2316870385736f7a58db9d986e21124a
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-02-13 23:35:25 +0530

    Split pane, tab switching btween code and preview, Export to Github tab

--- src/app/features/CodeEditor/projectIDView.tsx
+++ src/app/features/CodeEditor/projectIDView.tsx
@@ -2,6 +2,8 @@
 
 import { cn } from "@/lib/utils";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Tab = ({
     label,
@@ -14,11 +16,12 @@ const Tab = ({
 
 })=>{
 
+
     return(
         <div
         onClick={onClick}
         className={cn(
-            "flex items-center gap-2 h-full px-3 cursor-pointer text-muted-foreground border-r hover:bg-accent/30",
+            "flex items-center gap-2 h-full px-3 cursor-pointer text-muted-foreground border-r  hover:bg-accent/30",
             isActive && "bg-background text-foreground"
         )}
         >
@@ -36,21 +39,67 @@ export const ProjectIDView = (
 
     })=>{
 
+        const [activeView, setActiveView] = useState<"editor"|"preview">("editor")
+
         return(
 
             <div className="h-full flex flex-col">
                 <nav className="h-8.75 flex items-center bg-sidebar/80 border-b">
                     <Tab 
-                    label="Code"
-                    isActive={false}
-                    onClick={() => {}}
+                    label="Code </>"
+                    isActive={activeView === "editor"}
+                    onClick={() => setActiveView("editor")}
                     />
                     <Tab 
-                    label="Preview"
-                    isActive={false}
-                    onClick={() => {}}
+                    label="Preview 🌐"
+                    isActive={activeView === "preview"}
+                    onClick={() => setActiveView("preview")}
                     />
+
+                    <div className="flex-1 flex justify-end h-full">
+
+                        <div className="flex items-center gap-1.5 h-full px-3 cursor-pointer text-muted-foreground border-l hover:bg-accent/30">
+
+                            <FaGithub className="size-4.5" />
+
+                            <span className="text-sm">
+                                Export
+                            </span>
+                        </div>
+
+
+                    </div>
                 </nav>
+
+                <div className="flex-1 relative">
+
+                    <div className={cn(
+                        "absolute inset-0",
+                        activeView === "editor" ? "visible" : 
+                        "invisible"
+                    )}>
+
+                        <div>
+                            Editor
+                        </div>
+
+                    </div>
+
+                    <div className={cn(
+                        "absolute inset-0",
+                        activeView === "preview" ? "visible" : 
+                        "invisible"
+                    )}>
+
+                        <div>
+                            Preview
+                        </div>
+
+                    </div>
+
+                </div>
+
+            
             </div>
         )
 
