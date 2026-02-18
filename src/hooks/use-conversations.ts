e5f1e4cc0b3b152e996d5177561bce3e03cc3e3d
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-02-18 19:08:39 +0530

    Required hooks to use the conversation data

---
+++ src/hooks/use-conversations.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQuery } from "convex/react";
+
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+
+
+export const useConversation = (id: Id<"conversations">|null) => {
+    return useQuery(api.conversations.getById, id ? {id}: "skip");
+}
+
+export const useMessages = (conversationId: Id<"conversations">|null) => {
+    return useQuery
+    (api.conversations.getMessages,
+        conversationId ? {conversationId} : "skip"
+    );
+}
+
+export const useAllConversations = (projectId: Id<"projects">) => {
+    return useQuery (api.conversations.getByProject, {projectId});
+}
+
+
+export const useCreateConversation = () => {
+    return useMutation(api.conversations.create)
+
+    //TODO: add optimistic mutation
+}
